193c0235438d902f1fb325dad4a999fd824b87fe
Yibaebi <elliot.yibaebi@yahoo.com>
2020-12-15 16:57:48 +0100

    Elliot's initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Chidi has a big head
+
+function printChidisHead(head) {
+  return `I am Chidi and I have a big ${head}`;
+}
+
+const chidiHeadSize = printChidisHead("head");
+
+alert(chidiHeadSize);
