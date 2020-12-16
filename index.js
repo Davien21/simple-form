3a63cc3b95ab6daec8dbf01fb4a2ff14fe9d8f35
Yibaebi <elliot.yibaebi@yahoo.com>
2020-12-16 16:35:22 +0100

    changes from elliot

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Chidi has a big head
+// FirstName, LastName, Gender, Email, Password
+// Will recieve token
+
+
 
 function printChidisHead(head) {
   return `I am Chidi and I have a big ${head}`;
